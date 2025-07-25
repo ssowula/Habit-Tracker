d2e3701c7061dabcfeb7aecf26142cec2df9be9d
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-26 01:17:25 +0200

    create AppDbContext.cs model

---
+++ Habit-Tracker/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Habit_Tracker.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Habit-Tracker/Models/User.cs
+++ Habit-Tracker/Models/User.cs
@@ -1,5 +1,6 @@
 public class User
 {
+    public int Id { get; set; }
     public string? UserName { get; set; }
     public string? PasswordHash { get; set; } = string.Empty; 
 }
