de1b05d2a03d3d407d70f31e7034292778f7ac55
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-26 01:38:15 +0200

    start /login endpoint

--- Habit-Tracker/Program.cs
+++ Habit-Tracker/Program.cs
@@ -29,4 +29,14 @@ app.MapPost("/register", async (string? username, string? password,AppDbContext 
     return Results.Created($"/users/{user.Id}", new { user.Id, user.Username });
 });
 
+app.MapPost("/login", async (string? username, string? password, AppDbContext db) =>
+{
+    if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+    {
+        return Results.BadRequest("Username and password can't be empty");
+    }
+    var existingUser = await db.Users.FirstOrDefaultAsync(u => u.Username == username);
+    return Results.Ok();
+
+});
 app.Run();
