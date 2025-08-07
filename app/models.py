eb71a20ca34b1bc0b68d8c8a0497226c58bb82e3
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-08-07 22:49:27 +0200

    define database models for User, Habit, and HabitCompletion

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from datetime import datetime, date
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(20), unique=True, index=True)
+    password = Column(String)
+
+    habits = relationship("Habit", back_populates="owner")
+
+class Habit(Base):
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), index=True)
+    frequency = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="habits")
+    completions = relationship("HabitCompletion", back_populates="habit")
+
+class HabitCompletion(Base):
+    __tablename__ = "habit_completions"
+    id = Column(Integer, primary_key=True, index=True)
+    habit_id = Column(Integer, ForeignKey("habits.id"))
+    completion_date = Column(Date, default=date.today)
+
+    habit = relationship("Habit", back_populates="completions")
