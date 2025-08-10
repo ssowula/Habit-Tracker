ede39c84670d20b65737d7455786145df5721032
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-08-10 22:09:41 +0200

    add get_habits function

--- app/crud.py
+++ app/crud.py
@@ -16,8 +16,11 @@ def create_user(db: Session, user: schemas.UserCreate):
 def create_habit(db: Session, habit: schemas.HabitCreate, user_id: int):
     name = habit.name
     frequency = habit.frequency
-    db_habit = models.Habit(name=name,frequency=frequency,owner_id=user_id)
+    db_habit = models.Habit(name=name,frequency=frequency,user_id=user_id)
     db.add(db_habit)
     db.commit()
     db.refresh(db_habit)
     return db_habit
+
+def get_habits(db: Session, user_id: int):
+    return db.query(models.Habit).filter(models.Habit.user_id == user_id).all()
