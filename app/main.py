eccb9e417f10184153d98153d6e04075bebea42e
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-08-10 16:30:23 +0200

    add /users/me endpoint and configure CORS

--- app/main.py
+++ app/main.py
@@ -1,19 +1,34 @@
-from fastapi import FastAPI, Depends, HTTPException,status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from . import schemas,dependencies,crud,security,models
+from fastapi.middleware.cors import CORSMiddleware
+from . import crud, models, schemas, security, dependencies
 from .database import engine
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.post("/register",response_model=schemas.User, status_code=status.HTTP_201_CREATED)
-def user_register(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
-    if crud.get_user_by_username(db=db,username=user.username):
-        raise HTTPException(status_code=400, detail="Taki użytkownik już istnieje.")
-    return crud.create_user(db=db,user=user)
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "null",
+]
 
-@app.post("/login")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/register", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+def user_register(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
+    if crud.get_user_by_username(db=db, username=user.username):
+        raise HTTPException(status_code=400, detail="Taki użytkownik już istnieje.")
+    return crud.create_user(db=db, user=user)
+
+@app.post("/token")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(dependencies.get_db)):
     db_user = crud.get_user_by_username(db=db, username=form_data.username)
     if not db_user or not security.verify_password(form_data.password, db_user.password):
@@ -23,6 +38,10 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
             headers={"WWW-Authenticate": "Bearer"},
         )
     token = security.create_access_token(
-        {"sub": form_data.username}
+        data={"sub": db_user.username}
     )
     return {"access_token": token, "token_type": "bearer"}
+
+@app.get("/users/me", response_model=schemas.User)
+def read_users_me(current_user: models.User = Depends(dependencies.get_current_user)):
+    return current_user
