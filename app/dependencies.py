a0d456e0c859e9154d2ed2a1dff99b12bab6b362
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-08-10 16:25:19 +0200

    add get_current_user function

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,11 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from . import crud, schemas, config
 from .database import SessionLocal
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +13,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token,config.SECRET_KEY, algorithms=config.ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = crud.get_user_by_username(db=db,username=username)
+    if user is None:
+        raise credentials_exception
+    return user
